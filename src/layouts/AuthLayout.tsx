6178e3114979639634bbefe36308d8338a873098
venoajie <59521823+venoajie@users.noreply.github.com>
2025-11-02 11:25:47 +0700

    Update AuthLayout.tsx

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,28 +1,9 @@
 /** src/layouts/AuthLayout.tsx */
 
-import { Center, Container } from '@mantine/core';
-import { Outlet } from 'react-router-dom';
-
-/**
- * A simple, centered layout for public-facing pages like login, register, etc.
- */
-function AuthLayout() {
-  return (
-    <Container>
-      <Center style={{ height: '100vh' }}>
-        <Outlet />
-      </Center>
-    </Container>
-  );
-}
-
-export default AuthLayout;
-
 import {
   ActionIcon,
   AppShell,
   Center,
-  Container,
   Group,
   Title,
   useMantineColorScheme,
